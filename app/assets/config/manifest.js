a3519f8a7de3dd1dabaea1f1b35053cdaff0b46b
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2024-11-18 09:55:56 -0300

    importmap installed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,5 @@
 //= link_tree ../builds
 // app/assets/config/manifest.js
 //= link popper.js
-//= link bootstrap.min.js
+//= link bootstrap.min.js//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
